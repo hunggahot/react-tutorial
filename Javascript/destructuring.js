0d6b08b67fbf995c65e14bef64f5e532b0406e5c
hunggahot <hunggahot@gmail.com>
2023-07-01 21:45:37 +0700

    destructuring, rest

--- Javascript/destructuring.js
+++ Javascript/destructuring.js
@@ -1,9 +1,41 @@
-// Destructuring: phân rã, lấy trực tiếp Element bên trong mà không cần đi qua Array
+// Destructuring: phân rã, lấy trực tiếp Element bên trong mà không cần đi qua Array trả chúng dưới dạng String
+// Rest: được sử dụng với Destructuring
 
 var array = ['Javascript', 'PHP', 'Ruby']
 
-var a = array[0]
-var b = array[1]
-var c = array[2]
+var [a, c] = array
 
-console.log(a, b, c)
+console.log(a, c)
+
+var [a, b, ...rest] = array
+
+console.log(rest)
+
+var course = {
+    name: 'Javascript',
+    price: 1000,
+    children: {
+        name: 'ReactJS'
+    }
+}
+
+var { name, ...rest } = course
+
+console.log(rest)
+
+var { name: parentName, children: { name } } = course
+
+console.log(parentName)
+console.log(name)
+
+const {
+    x = 'default',
+    y,
+    ...param
+} = {
+    y: 'val1',
+    z: 'val2',
+    w: 'val3'
+};
+
+console.log(x, y, param);
