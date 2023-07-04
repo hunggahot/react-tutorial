1429991a102187da17298fd6fb959562e8fd76ac
hunggahot <hunggahot@gmail.com>
2023-07-04 17:37:00 +0700

    useState

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+const orders = [100, 200, 300]
 
 function App() {
+
+  const [counter, setCounter] = useState(() => {
+    const total = orders.reduce((total, cur) => total + cur)
+
+    console.log(total)
+    return total
+  })
+
+  const handleIncrease = () => {
+    setCounter(prevState => prevState + 1)
+  }
+
+  const [info, setInfo] = useState({
+    name: 'Lam Quoc Hung',
+    age: 22,
+    address: 'HCM, VN'
+  })
+
+  const handleUpdate = () => {
+    setInfo({
+      ...info,
+      bio: 'Yeu mau hong ^^'
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      <h1>{counter}</h1>
+      <button onClick={handleIncrease}>Increase</button>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={handleUpdate}>Update</button>
     </div>
   );
 }
